802cd54fcd97568c35bd58a716c226e25918d5d4
Ehsan Sam <ehsan_sm@ymail.com>
2021-01-13 00:29:11 +0330

    single neuron

--- simple Neural network.py
+++ simple Neural network.py
@@ -6,6 +6,17 @@ ActivationFunc = Callable[[float],float]
 def neuron(inputs: Vector, weights: Vector, activation_func : ActivationFunc) ->float:
     return activation_func(
         sum(
-            z[0*z[1] for z in zip([1.0]+inputs, weights)]
+            z[0]*z[1] for z in zip([1.0]+inputs, weights)
         )
     )
+#the activation function :
+def step(x: float) -> float:
+    return 1 if x>0 else 0
+
+weights = [-0.25, 1, 0.45]
+
+glen = [-0.20,0.18]
+tal = [0.6,-0.41]
+
+print("glen", neuron(glen,weights,step))
+print("tal", neuron(tal,weights,step))
